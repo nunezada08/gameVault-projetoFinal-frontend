fe3ed10fa06391fb149362a471801202ea044cab
Henrico <henrico.ferreira@aluno.senai.br>
2025-11-07 14:06:29 -0300

    fix: arruma os scripts de cadastro e login

--- assets/scripts/cadastroScript.js
+++ assets/scripts/cadastroScript.js
@@ -1,12 +1,8 @@
-document.getElementById('infos').addEventListener('click', function() {
+document.getElementById('btns').addEventListener('click', function() {
 
-    const usuario = document.getElementById('infoNome').value;
-    const dataNasc = document.getElementById('infoData').value;
     const email = document.getElementById('infoMail').value;
     const senha = document.getElementById('infoSenha').value;
 
-    localStorage.setItem('usuario', usuario);
-    localStorage.setItem('dataNasc', dataNasc);
     localStorage.setItem('senha', senha);
     localStorage.setItem('email', email);
 
